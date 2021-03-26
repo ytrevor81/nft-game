e435635209daa35087e50316fca4404313322b30
Trevor Youngquist <ytrevor81@gmail.com>
2021-03-26 15:36:55 +0700

    nft deployed on rinkeby

--- migrations/2_deploy_flareoncontract.js
+++ migrations/2_deploy_flareoncontract.js
@@ -4,6 +4,6 @@ const config = require('../config.json');
 module.exports = async function(deployer) {
   await deployer.deploy(Flareon);
   const nft = await Flareon.deployed();
-  //await nft.mintNft(config.RINKEBY_ADDRESS, config.FLAREON_METADATA);
-  await nft.mintNft("0x361eccb8E577800Aa2d60994773dE975E28a68CE", config.FLAREON_METADATA); //Ganache address
+  await nft.mintNft(config.RINKEBY_ADDRESS, config.FLAREON_METADATA);
+  //await nft.mintNft("0x361eccb8E577800Aa2d60994773dE975E28a68CE", config.FLAREON_METADATA); //Ganache address
 };
